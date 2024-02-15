fa803f488be44f9db12c7ee45474e3f375e0352f
azuda <aaronzhang587@gmail.com>
2024-02-15 13:30:11 -0800

    Added option to nuke volume

--- docker_nuke.ps1
+++ docker_nuke.ps1
@@ -1,3 +1,8 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q) -f
 docker rmi $(docker images -q 'docker_video_streaming*')
+
+$UserInput = Read-Host "Nuke volume? [y/n]"
+if ($UserInput -eq "y") {
+  docker volume rm docker_video_streaming_shared_volume -f
+}
